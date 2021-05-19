5fac4e11d1014e23a83bc445e0587d160bfc7776
codekrnl <eyalb02@gmail.com>
2021-05-19 20:55:44 +0300

    Change template items from .html to .jinja, fix templates folder not being loaded automatically

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ from data.data_analyzer import get_data, get_summary
 from main import WEB_PORT
 
 template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-template_dir = os.path.join(template_dir, 'bandwidth-monitor-master')
+template_dir = os.path.join(template_dir, 'bandwidth-monitor')
 template_dir = os.path.join(template_dir, 'web')
 template_dir = os.path.join(template_dir, 'templates')
 app = Flask(__name__, template_folder=template_dir)
@@ -18,7 +18,7 @@ started = None
 
 @app.route('/settings')
 def settings():
-    template = render_template('settings.html')
+    template = render_template('settings.jinja')
 
     return template
 
@@ -26,7 +26,7 @@ def settings():
 def index():
     global started
 
-    current_values = get_data(f'select time_timestamp, download_rate from bandwidth_data order by time_timestamp asc')
+    current_values = get_data(f'select time_timestamp, download_rate from bandwidth_data where data_source="speedtest.net" order by time_timestamp asc')
 
     data = []
     categories = []
@@ -41,10 +41,11 @@ def index():
 
     requests, download_rate_avg, upload_rate_avg = get_summary(time_started=started)
 
-    template = render_template('index.html', categories=categories, data=data, requests=requests,
+    template = render_template('index.jinja', categories=categories, data=data, requests=requests,
                                download_rate_avg=download_rate_avg, upload_rate_avg=upload_rate_avg,
                                start_time=parse_datetime(started),
                                this_time=parse_datetime(datetime.datetime.now().timestamp()))
+
     return template
 
 
@@ -53,10 +54,15 @@ def parse_datetime(f):
 
 
 def main(web_port):
+    global template_dir
+
     print('[INFO]: Starting web server...')
 
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.CRITICAL)
 
     app.env = 'production'
+    print(f'[INFO]: Starting server will be listening on port: http://localhost:{web_port}.')
+
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=False, port=web_port)
